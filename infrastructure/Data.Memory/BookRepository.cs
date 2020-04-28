71480ee486a54c2620e27d64eb4cef8661c4b1ff
Mark Shevchenko <markshevchenko@gmail.com>
2020-04-28 16:59:24 +0300

    Реализовать BookRepository

---
+++ infrastructure/Data.Memory/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using Store;
+
+namespace Data.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art Of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
+
+        public Book[] GetAllByTitle(string title)
+        {
+            return books;
+        }
+    }
+}
